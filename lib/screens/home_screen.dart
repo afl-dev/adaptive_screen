2e69d16c4868f86a0a469ac2ffffe8452bd97675
Alexander L <sta26@yandex.ru>
2020-05-26 01:46:23 +0300

    исправлен импорт home_screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:page_view_indicator/page_view_indicator.dart';
-import 'file:///C:/Users/admin/IdeaProjects/ra/lib/widgets/divider_vert_widget.dart';
-import 'file:///C:/Users/admin/IdeaProjects/ra/lib/repository/dummy_data.dart';
-
+import 'package:ra/repository/dummy_data.dart';
+import 'package:ra/widgets/divider_vert_widget.dart';
 
 class HomeScreen extends StatefulWidget {
-
   const HomeScreen({
     Key key,
   }) : super(key: key);
@@ -494,7 +492,6 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ),
       ),
-      // bottomNavigationBar: _getNavigationBar(context),
       bottomNavigationBar: Container(
         decoration: BoxDecoration(
           borderRadius: BorderRadius.only(
